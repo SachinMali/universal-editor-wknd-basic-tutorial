fc40bc87f2a1065be87a8331378ddd35d9de185c
Sachin Mali <sachinpree@gmail.com>
2024-04-15 19:35:52 -0400

    UE-Prep: added UE script and page instrumentation

--- src/App.js
+++ src/App.js
@@ -13,23 +13,39 @@ import logo from "./images/wknd-icon.svg";
 import Home from "./components/Home";
 import Person from "./components/Person";
 import "./App.scss";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <Router>
+    <HelmetProvider>
       <div className="App">
-        <header>
-          <Link to={"/universal-editor-wknd-basic-tutorial"}>
-            <img src={logo} className="logo" alt="WKND Logo" />
-          </Link>
-          <hr />
-        </header>
-        <Routes>
-          <Route path="/universal-editor-wknd-basic-tutorial" element={<Home />} />
-          <Route path="/person/:fullName" element={<Person />} />
-        </Routes>
+        <Helmet>
+          <script
+            src="https://universal-editor-service.experiencecloud.live/corslib/LATEST"
+            async
+          />
+          <meta
+            name="urn:adobe:aue:system:aemconnection"
+            content={`aem:https://author-p46652-e1315962.adobeaemcloud.com`}
+          />
+        </Helmet>
+        <Router>
+          <header>
+            <Link to={"/universal-editor-wknd-basic-tutorial"}>
+              <img src={logo} className="logo" alt="WKND Logo" />
+            </Link>
+            <hr />
+          </header>
+          <Routes>
+            <Route
+              path="/universal-editor-wknd-basic-tutorial"
+              element={<Home />}
+            />
+            <Route path="/person/:fullName" element={<Person />} />
+          </Routes>
+        </Router>
       </div>
-    </Router>
+    </HelmetProvider>
   );
 }
 
